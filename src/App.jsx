e14cbeb1f531bcc118ed55cd879ad85baa992335
Cedric Naño <146691376+cedik456@users.noreply.github.com>
2024-10-19 16:00:03 +0800

    Finished creating a digital clock using react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import DigitalClock from "./DigitalClock";
+
+function App() {
+  return (
+    <div>
+      <DigitalClock />
+      <DigitalClock />
+      <DigitalClock />
+      <DigitalClock />
+      <DigitalClock />
+    </div>
+  );
+}
+
+export default App;
